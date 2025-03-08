5e62aefe0f1dfc7b85784c7f10463cdfe8b7975a
Sar1h <sarthakbh009@gmail.com>
2025-03-09 03:30:22 +0530

    data ingestion bug fix

--- TG/app/routes/shipment-details.tsx
+++ TG/app/routes/shipment-details.tsx
@@ -456,7 +456,8 @@ export default function ShipmentDetailsPage() {
                       value={formData.shipmentDate}
                       onChange={handleInputChange}
                       className="mt-1 block w-full rounded-md border border-[#E2E8F0] py-2 px-3 text-[#1E293B]
-                        focus:border-[#1E40AF] focus:ring focus:ring-[#1E40AF]/20 bg-white"
+                        focus:border-[#1E40AF] focus:ring focus:ring-[#1E40AF]/20 bg-white
+                        [color-scheme:light]"
                       required
                     />
                   </div>
